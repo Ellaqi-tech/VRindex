1376a91a8f7bb9701e56ee6f4c3de926400f5670
Ellaqi <ellaqi77777@gmail.com>
2020-06-12 11:50:23 -0400

    finished input interact

--- client.js
+++ client.js
@@ -1,26 +1,56 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
-
 import {ReactInstance, Surface, Location} from 'react-360-web';
+import {Module} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
+
+  class CustomLinkModule extends Module {
+    constructor() {
+      super('CustomLinkModule'); // Makes this module available at NativeModules.MyModule
+    }
+    openInNewTab(url) {     // This method will be exposed to the React app
+      window.open(url, '_blank');
+    }
+
+  };
+
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
+    //NativeModules:
+    //Register custom modules at init time
+    nativeModules: [
+      new CustomLinkModule(),
+    ],
     ...options,
   });
 
+// const r360 = new ReactInstance(bundle, parent, {
+//   // Add custom options here
+//   fullScreen: true,
+//   ...options,
+// }
+// );
 
-
-
+   //NativeModules:
+  // Register custom modules at init time
+//   const r360Module = new ReactInstance(
+//     'VRindex.bundle?platform=vr',
+//     document.getElementById('container'), {
+//     nativeModules: [
+//       new CustomLinkModule(),
+//     ]
+//   }
+// );
 
   //left panel
   const leftPanel = new Surface(
-    1200,
-    600,
-    Surface.SurfaceShape.Flat
+    3700,
+    700,
+    Surface.SurfaceShape.Cylinder
   );
-  leftPanel.setAngle(-1.51, 0);
+  leftPanel.setAngle(-60, 0);
   r360.renderToSurface(
     r360.createRoot('leftsurface'),
     leftPanel,
@@ -28,7 +58,7 @@ function init(bundle, parent, options = {}) {
   
   //right panel
   const rightPanel = new Surface(
-    1200,
+    1000,
     600,
     Surface.SurfaceShape.Flat
   );
@@ -38,20 +68,11 @@ function init(bundle, parent, options = {}) {
     rightPanel,
   )
 
-
-
-
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
     r360.createRoot('VRindex', { /* initial props */ }),
     r360.getDefaultSurface()
   );
-
-  // r360.renderToSurface(
-  //   r360.createRoot('ViewColoredBoxesWithText', { /* initial props */ }),
-  //   r360.getDefaultSurface()
-  // );
-
 
   // Load the initial environment
   r360.compositor.setBackground(r360.getAssetURL('nikita-taparia-wRcuI89O-_Q-unsplash.jpg',{ format: '2D', } //specify the layout of the background image with a second options argument.
--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 import * as React from 'react';
+//import 'react-native';
 import {AppRegistry,StyleSheet,Text,View,VrButton,Image} from 'react-360';
-import { YellowBox } from 'react-native';
-//import {connect, setCurrent} from './Store';
+import {NativeModules} from 'react-360';
 
+//native modules
+const {CustomLinkModule} = NativeModules.LinkingManager;
+
+//greeting surface
 export default class VRindex extends React.Component {
   render() {
     return (
-      // greeting panel
+      // greeting panel      
       <View style={styles.panel}>
+        {/* background image */}
+        <View style={styles.greetingBox}>
+          <Image style={styles.greetingbg}
+            source={require('./static_assets/blog-cover-ps.jpg')} />
+        </View>
+        {/* arrow icons */}
+        <View style={styles.greetingIconContainerLeft}>
+          <Image style={styles.iconimg}
+            source={require('./static_assets/left-arrow.png')} />
+        </View>
+        <View style={styles.greetingIconContainerRight}>
+          <Image style={styles.iconimg}
+            source={require('./static_assets/right-arrow.png')} />
+        </View>
+
         <View style={styles.greetingtextContainer}>
           <Text style={styles.greetingtext}>
             Good designs, not just beautiful
           </Text>
         </View>
-        {/* background image */}
-        <View style={styles.greetingBox}>
-          <Image style={{
-              resizeMode: "cover",
-              width: 1000,
-              height: 800,
-              opacity: 0.5,
-              zIndex: 0,
-            }}
-            source={require('./static_assets/blog-cover-ps.jpg')} />
-        </View>
-        {/* arrow icons */}
-        <View style={styles.greetingIconContainerLeft}>
-          <Image style={{
-              resizeMode: "cover",
-              width: 40,
-              height: 40,
-            }}
-            source={require('./static_assets/left-arrow.png')} />
-        </View>
-        <View style={styles.greetingIconContainerRight}>
-          <Image style={{
-              resizeMode: "cover",
-              width: 40,
-              height: 40,
-            }}
-            source={require('./static_assets/right-arrow.png')} />
-        </View>
+
+        <VrButton style={styles.backlink} onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://ellaqi.com/')}}>
+          <View style={styles.linkView}>
+            <Text style={styles.backLinkText}>Click to back to flat world</Text>
+          </View>
+        </VrButton>
+        
       </View>
     );
   }
@@ -50,197 +48,102 @@ export default class VRindex extends React.Component {
 export class leftsurface extends React.Component {
   render() {
     return (
-      <View style={styles.rightpanel}>
-        <View style={styles.rcontainer}>
-          {/* title */}
-          <View>
-            <Text>Web Projects</Text>
-          </View>
+      <View style={styles.leftpanel}>
+        <View style={styles.lcontainer}>
 
-          <View>
-            <Text>Personal Projects</Text>
-          </View>
+          {/* <View style={styles.labelView}>
+            <Text style={styles.labelText}>Personal Projects</Text>
+          </View> */}
 
           {/* flex container for each row */}
-          <View style={{
-            flex:1,
-            flexDirection: 'row',
-          }}>
-
-              {/* box for each project */}
-              <View style={{
-                flex:1,
-                flexDirection: 'colunm',
-              }}>
-                <View style={{
-                  width: 200,
-                  height: 160,
-                }}>
-                  <Image style={{
-                      resizeMode: "cover",
-                      width: 200,
-                      height: 155,
-                    }}
+          <View style={styles.projectFlexBox}>
+            {/* box for each project */}
+            <View style={styles.projectFlexCol}>
+              <VrButton onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://qeclock.ellaqi.com/')}}>
+                <View style={styles.projectImgView}>
+                  <Image style={styles.projectImg}
                     source={require('./static_assets/qeclockF.png')} />
                 </View>
-                <View style={{
-                  width: 200,
-                  height: 100,
-                }}>
-                  <Text style={styles.projectText}>Q.E.Clock{"\n"}
-                  Get more detail
-                  </Text>
-                </View>
+              </VrButton>
+              <View style={styles.projectTextView}>
+                <Text style={styles.projectText}>Q.E.Clock</Text> 
+                {/* {"\n"} */}
               </View>
-
-
-              {/* box for each project */}
-              <View style={{
-                flex:1,
-                flexDirection: 'colunm',
-              }}>
-                <View style={{
-                  width: 200,
-                  height: 160,
-                }}>
-                  <Image style={{
-                      resizeMode: "cover",
-                      width: 200,
-                      height: 155,
-                    }}
+            </View>
+            {/* box for each project */}
+            <View style={styles.projectFlexCol}>
+              <VrButton onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://qerecipe.ellaqi.com/')}}>
+                <View style={styles.projectImgView}>
+                  <Image style={styles.projectImg}
                     source={require('./static_assets/qerecipeF.png')} />
-                </View>
-                <View style={{
-                  width: 200,
-                  height: 100,
-                }}>
-                  <Text style={styles.projectText}>Q.E.Recipe{"\n"}
-                    Get more detail
-                  </Text>
-                </View>
+                  </View>
+              </VrButton>
+              <View style={styles.projectTextView}>
+                <Text style={styles.projectText}>Q.E.Recipe</Text>
               </View>
-
-              {/* box for each project */}
-              <View style={{
-                flex:1,
-                flexDirection: 'colunm',
-              }}>
-                <View style={{
-                  width: 200,
-                  height: 160,
-                }}>
-                  <Image style={{
-                      resizeMode: "cover",
-                      width: 200,
-                      height: 155,
-                    }}
+            </View>
+            {/* box for each project */}
+            <View style={styles.projectFlexCol}>
+              <VrButton onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://pokemongame.ellaqi.com/')}}>
+                <View style={styles.projectImgView}>
+                  <Image style={styles.projectImg}
                     source={require('./static_assets/pokemonF.png')} />
                 </View>
-                <View style={{
-                  width: 200,
-                  height: 100,
-                }}>
-                  <Text style={styles.projectText}>Run Pokemon!{"\n"}
-                    Get more detail
-                  </Text>
+                </VrButton>
+                <View style={styles.projectTextView}>
+                  <Text style={styles.projectText}>Run Pokemon!</Text>
                 </View>
               </View>
-          </View>
+           </View>
 
-          <View>
-            <Text>Team Projects</Text>
-          </View>
+          {/* <View style={styles.labelView}>
+            <Text style={styles.labelText}>Team Projects</Text>
+          </View> */}
 
           {/* flex container for each row */}
-          <View style={{
-            flex:1,
-            flexDirection: 'row',
-          }}>
+          <View style={styles.projectFlexBox}>
 
-              {/* box for each project */}
-              <View style={{
-                flex:1,
-                flexDirection: 'colunm',
-              }}>
-                <View style={{
-                  width: 200,
-                  height: 160,
-                }}>
-                  <Image style={{
-                      resizeMode: "cover",
-                      width: 200,
-                      height: 155,
-                    }}
-                    source={require('./static_assets/tdF.png')} />
-                </View>
-                <View style={{
-                  width: 200,
-                  height: 100,
-                }}>
-                  <Text style={styles.projectText}>TicketDodger{"\n"}
-                  Get more detail
-                  </Text>
-                </View>
+            <View style={styles.projectFlexCol}>
+            <VrButton onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://chrisjones.codes/ticketdodger/')}}>
+              <View style={styles.projectImgView}>
+                <Image style={styles.projectImg}
+                  source={require('./static_assets/tdF.png')} />
               </View>
-
-              {/* box for each project */}
-              <View style={{
-                flex:1,
-                flexDirection: 'colunm',
-              }}>
-                <View style={{
-                  width: 200,
-                  height: 160,
-                }}>
-                  <Image style={{
-                      resizeMode: "cover",
-                      width: 200,
-                      height: 155,
-                    }}
-                    source={require('./static_assets/bbqhome.png')} />
-                </View>
-                <View style={{
-                  width: 200,
-                  height: 100,
-                }}>
-                  <Text style={styles.projectText}>BBQ{"\n"}
-                    Get more detail
-                  </Text>
-                </View>
-                </View>
-
-                {/* box for each project */}
-                <View style={{
-                  flex:1,
-                  flexDirection: 'colunm',
-                }}>
-                  <View style={{
-                    width: 200,
-                    height: 160,
-                  }}>
-                    <Image style={{
-                        resizeMode: "cover",
-                        width: 200,
-                        height: 155,
-                      }}
-                      source={require('./static_assets/berryF.png')} />
-                  </View>
-                  <View style={{
-                    width: 200,
-                    height: 100,
-                  }}>
-                    <Text style={styles.projectText}>BerryTeam{"\n"}
-                      Get more detail
-                    </Text>
-                  </View>
-                </View>
+              </VrButton>
+              <View style={styles.projectTextView}>
+                <Text style={styles.projectText}>TicketDodger</Text>
               </View>
+            </View>
+
+            <View style={styles.projectFlexCol}>
+            <VrButton onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://2019restaurant.ellaqi.com/')}}>
+              <View style={styles.projectImgView}>
+                <Image style={styles.projectImg}
+                  source={require('./static_assets/bbqhome.png')} />
+              </View>
+              </VrButton>
+              <View style={styles.projectTextView}>
+                <Text style={styles.projectText}>Pompeii BBQ Restaurant</Text>
+              </View>
+            </View>
+
+            <View style={styles.projectFlexCol}>
+            <VrButton onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://berryteam-4u.com/MotivationQuote/motivation.php')}}>
+              <View style={styles.projectImgView}>
+                <Image style={styles.projectImg}
+                  source={require('./static_assets/berryF.png')} />
+              </View>
+              </VrButton>
+              <View style={styles.projectTextView}>
+                <Text style={styles.projectText}>BerryTeam</Text>
+              </View>
+            </View>
+          </View>
         </View>
       </View>
     );
   }
-}
-
+};
 
 // example for creating a View that wraps two colored boxes and a text component in a row with padding.
 export class rightsurface extends React.Component {
@@ -254,21 +157,83 @@ export class rightsurface extends React.Component {
           <Text style={styles.righttext}>Ella Qi is a web developer, designer, painter with a background in design and communication engineering. She is passionate about designing beautiful websites' visual effects and targeted in creating functional and creative websites. It is proud that she is doing the work she dreamed of when she was eight years old. 
             {"\n"}Contact her by
           </Text>
+          
+          {/* <VrButton style={styles.rightbutton} onClick={e =>{NativeModules.LinkingManager.openURL('https://ellaqi.com/contact-me/')}}></VrButton> */}
+          <View style={{paddingTop: 15, paddingLeft: 50, paddingBottom: 15}}>
+            <VrButton style={styles.rightbutton} onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('me@ellaqi.com')}}>
+              <Text style={styles.righttext}>me@ellaqi.com (email)</Text>
+            </VrButton>
+            <VrButton style={styles.rightbutton} onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://www.linkedin.com/in/ella-qi-b09037177/')}}>
+              <Text style={styles.righttext}>LinkedIn</Text>
+            </VrButton>
+            <VrButton style={styles.rightbutton} onClick={e =>{NativeModules.CustomLinkModule.openInNewTab('https://github.com/Ellaqi-tech')}}>
+              <Text style={styles.righttext}>GitHub</Text>
+            </VrButton>
+          </View>
+
           <Text style={styles.rightSmallText}>She is not very busy lately, especially during the quarantine. So just contact her.</Text>
+          <Text style={styles.rightSmallText}>Click on the links to see what could happed</Text>
+          
         </View>
       </View>
     );
   }
-}
-              {/* <VrButton style={styles.rightbutton}>me@ellaqi.com</VrButton>
-              <VrButton style={styles.rightbutton}>LinkedIn</VrButton>
-              <VrButton style={styles.rightbutton}>GitHub</VrButton> */}
-
-
-
-
+};
 
 const styles = StyleSheet.create({
+  iconimg: {
+    resizeMode: "cover",
+    width: 40,
+    height: 40,
+    opacity: 0.90,
+  },
+  backLinkText: {
+    color: '#FFFFFF',
+  },
+  linkView: {
+    zIndex: 10,
+  },
+  greetingbg: {
+    resizeMode: "cover",
+    width: 1000,
+    height: 800,
+    position: 'absolute',
+    top:0,
+    left:0,
+    right:0,
+    bottom:0,
+    opacity: 0.75,
+    zIndex: -1,
+  },
+  backlink: {
+    borderColor: '#5B5A5A',
+    borderWidth: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.65)',
+    position: 'absolute',
+    top: 430,
+    padding: 10,
+    opacity: 1,
+    zIndex: 1,
+  },
+  shadowStyle: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    
+    elevation: 24,
+  },
+  shadowText: {
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 1},
+    textShadowRadius: 10,
+  },
+  textColor: {
+    color: '#C0C0C0',
+  },
   //greeting panel
   panel: {
     // Fill the entire surface
@@ -281,15 +246,15 @@ const styles = StyleSheet.create({
   },
   greetingtextContainer: {
     position: 'absolute',
-    top: 300,
+    top: 250,
     padding: 10,
     zIndex: 1,
-    backgroundColor: '#000000',
+    //backgroundColor: '#000000',
   },
   greetingtext: {
-    fontSize: 35,
+    fontSize:45,
     textAlign: 'center',
-    // color: '#000000',
+    color: '#FFFFFF',
     zIndex: 1,
   },
   greetingBox: {
@@ -298,7 +263,7 @@ const styles = StyleSheet.create({
     left: 0,
     padding: 0,
     margin: 0,
-    zIndex: 0,
+    zIndex: -1,
   },
  greetingIconContainerLeft: {
   position: 'absolute',
@@ -313,13 +278,15 @@ const styles = StyleSheet.create({
 
   //right panel 
   rightbutton: {
-    height: 120,
-    backgroundColor: '#000000',
+    width:300,
+    height: 30,
+    padding: 9,
   },
   righttext: {
     fontSize: 26,
-    color: 'white',
+    color: '#FFFFFF',
     lineHeight: 60,
+    fontWeight: 'bold',
   },
   righttitle: {
     fontSize: 60,
@@ -334,26 +301,76 @@ const styles = StyleSheet.create({
   },
   rightSmallText: {
     fontSize: 18,
+    fontWeight: 'bold',
+    paddingTop: 10,
   },
   rightpanel: {
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    backgroundColor: 'rgba(255, 255, 255, 0)',
     alignItems: 'center',
     zIndex: 0,
   },
   rcontainer: {
     width: 800,
     height: 600,
-    padding: 50,
+    padding: 20,
   },
 //right panel ends
 //left panel
-  projectText: {
-    fontSize: 14,
-    textAlign: 'center',
+leftpanel: {
+  width: 1400,
+  height: 700,
+  backgroundColor: 'rgba(255, 255, 255, 0)',
+  alignItems: 'center',
+  zIndex: 0,
+},
+lcontainer: {
+  width: 1080,
+  height: 680,
+  padding: 10,
+},
+projectImgView: {
+  width: 290,
+  height: 210,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+  projectImg: {
+      resizeMode: "cover",
+      width: 280,
+      height: 200,
   },
-
+  projectText: {
+    fontSize: 24,
+    textAlign: 'center',
+    color: '#ffffff',
+    fontWeight: 'bold',
+  },
+  projectFlexBox: {
+    flex:1,
+    flexDirection: 'row',
+    padding: 0,
+  },
+  projectFlexCol: {
+    flex:1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  projectTextView: {
+    width: 290,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  // labelView: {
+  //   width: 1080,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // labelText: {
+  //   fontWeight: 'bold',
+  //   fontSize: 24,
+  // }
 
 
   
