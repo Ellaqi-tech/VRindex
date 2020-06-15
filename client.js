af8a5a0a356251ecd43159a8099f1ba953cd1ee7
Ellaqi <ellaqi77777@gmail.com>
2020-06-15 00:49:39 -0400

    update bg path

--- client.js
+++ client.js
@@ -71,7 +71,7 @@ function init(bundle, parent, options = {}) {
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('./vrbg5.jpg',{ format: '2D', } //specify the layout of the background image with a second options argument.
+  r360.compositor.setBackground(r360.getAssetURL('././vrbg5.jpg',{ format: '2D', } //specify the layout of the background image with a second options argument.
   ));
   
 }
